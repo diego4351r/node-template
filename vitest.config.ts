7fd236ad2dc3607d581e2131144906526c3ab2d2
Diego <75797588+diego4351r@users.noreply.github.com>
2022-07-04 19:56:37 +0200

    Added vitest support for absolute imports

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  resolve: {
+    alias: [
+      { find: '@absolutePath', replacement: path.resolve(__dirname, 'src/absolutePath')}, //just an example
+    ]
+  }
+});
